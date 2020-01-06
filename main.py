302061b41ee6d410f924750ef8e0b747b2f27d23
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-01-06 10:03:01 +0000

    Added ConnectAutomation module imports

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import argparse
+from social_image_generator import SocialImageGenerator
+from sched_data_interface import SchedDataInterface
 
 def main(args):
     """Takes the argparse arguments as input and starts scripts"""
 
     print("Linaro Connect Automation Container")
+
+    # Determine the results of the args
+
+
+class AutomationContainer:
+    def __init__(self):
+        # Instatiate the ScehdDataInterface which is used by other modules for the data source
+        self.sched_data = SchedDataInterface()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Connect Automation")
