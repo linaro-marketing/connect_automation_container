c37786b248d455f4b564db23316b9c57a789957c
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-02-14 15:54:41 +0000

    Corrected the reviewers url format

--- app/github_manager.py
+++ app/github_manager.py
@@ -105,7 +105,8 @@ class GitHubManager:
             data = {
                 "reviewers": self.reviewers
             }
-            url = "https://api.github.com/repos/{}/pulls/{}/requested_reviewers".format(json["number"], self.github_repo_key)
+            url = "https://api.github.com/repos/{1}/pulls/{2}/requested_reviewers".format(
+                self.github_repo_key, json["number"])
 
             result = requests.post(url, json=data, headers=headers)
 
